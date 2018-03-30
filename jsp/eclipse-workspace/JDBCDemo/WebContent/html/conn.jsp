dea1bec88ebad5ad35bb2391189475e25f7e424c
vulcan-soundwave <1508040120@mails.qust.edu.cn>
2018-03-30 16:42:47 +0800

    update JDBCDemo to connect mysql

---
+++ jsp/eclipse-workspace/JDBCDemo/WebContent/html/conn.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<jsp:useBean id="db" class="mypackage.DbBean"/>
+<c:set target="${db}" property="jdbcUrl"
+	value="jdbc:mysql://localhost:3306/demo"/>
+<c:set target="${db}" property="username" value="vulcan"/>
+<c:set target="${db}" property="password" value="561520"/>
+<!DOCTYPE HTML PUBLIC "-//W3C/DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type"
+			content="text/html; charset=UTF-8">
+		<title>测试数据库连接</title>
+	</head>
+	<body>
+		<c:choose>
+			<c:when test="${db.connectedOK}">连接成功！</c:when>
+			<c:otherwise>连接失败！</c:otherwise>
+		</c:choose>
+	</body>
+</html>
+
---
+++ jsp/eclipse-workspace/JDBCDemo/src/mypackage/DbBean.java
@@ -0,0 +1,69 @@
+package mypackage;
+
+import java.sql.*;
+import java.io.*;
+
+public class DbBean implements Serializable {
+	
+	private String jdbcUrl;
+	private String username;
+	private String password;
+	
+	public DbBean() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch (ClassNotFoundException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	public boolean isConnectedOK() {
+		boolean ok = false;
+		Connection conn = null;
+		SQLException ex = null;
+		try {
+			conn = DriverManager.getConnection(jdbcUrl, username, password);
+			if (!conn.isClosed()) {
+				ok = true;
+			}
+		}
+		catch (SQLException e) {
+			ex =e;
+		}
+		finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					if (ex == null) {						
+						ex = e;
+					}
+				}
+			}
+			if (ex != null) {
+				throw new RuntimeException(ex);
+			}
+		}
+		return ok;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setJdbcUrl(String jdbcUrl) {
+		this.jdbcUrl = jdbcUrl;
+	}
+	
+	public void setUserName(String username) {
+		this.username = username;
+	}
+	
+	public static void main(String[] args) {
+
+		
+	}
+
+}
